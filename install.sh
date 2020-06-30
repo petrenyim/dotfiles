c7f42fc6276114e68fc1c8c1559256078f250b61
Mihaly Petrenyi <petrenyimisi@gmail.com>
2020-06-30 22:07:34 +0200

    Fix install script

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ fi
 apt install -y alacritty
 
 rm -f "/home/${SUDO_USER}/.config/alacritty/alacritty.yml"
-ln -s ./dotfiles/alacritty.yml "/home/${SUDO_USER}/.config/alacritty/alacritty.yml"
+ln -s "$(cd $(dirname ${BASH_SOURCE}) && pwd)/dotfiles/alacritty.yml" "/home/${SUDO_USER}/.config/alacritty/alacritty.yml"
