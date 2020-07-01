2092ae419378ae9a6bf57d893ad15b1af687a456
Mihaly Petrenyi <petrenyimisi@gmail.com>
2020-07-01 11:43:35 +0200

    Add alacritty ppa to install script

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ if [[ -z "${SUDO_USER}" ]]; then
 fi
 
 # Install alacritty on host
+add-apt-repository ppa:mmstick76/alacritty
+apt update
 apt install -y alacritty
 
+mkdir -p "/home/${SUDO_USER}/.config/alacritty"
 rm -f "/home/${SUDO_USER}/.config/alacritty/alacritty.yml"
 ln -s "$(cd $(dirname ${BASH_SOURCE}) && pwd)/dotfiles/alacritty.yml" "/home/${SUDO_USER}/.config/alacritty/alacritty.yml"
